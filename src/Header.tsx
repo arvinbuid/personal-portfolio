10e291392e0aa42acbd1c3b140a8b6eeb8cedaed
Arvin Buid <arvinbuid01@gmail.com>
2024-07-05 11:20:17 +0800

    Changed header left side from text to logo-icon.

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,10 +1,11 @@
 import {ModeToggle} from "./components/ui/mode-toggle";
 import {ThemeProvider} from "@/components/ui/theme-provider";
+import logo from "./assets/ab-logo.png";
 
 const Header = () => {
   return (
-    <nav className='m-auto flex items-center justify-between px-5 min-h-[80px] text-xl'>
-      <h2>Arvin Buid</h2>
+    <nav className='m-auto flex items-center justify-between px-5 min-h-[80px]'>
+      <img src={logo} width={60} className='cursor-pointer' />
       <ul className='flex items-center justify-center gap-5'>
         <li>
           <ThemeProvider defaultTheme='light' storageKey='vite-ui-theme'>
